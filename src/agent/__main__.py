149bbe9ff575936a8cb9db2711ddf6a87af7d2ab
markshao <shaoyuliang@qq.com>
2024-09-04 19:15:16 +0800

    loguru

--- src/agent/__main__.py
+++ src/agent/__main__.py
@@ -1,10 +1,23 @@
 from dotenv import load_dotenv
 
 load_dotenv()
+
+import sys
+from loguru import logger
+
 from agent.agent import BuffetAgent
 
 
+def init_logger():
+    logger.add(
+        sys.stdout,
+        colorize=True,
+        format="<green>{time}</green> <level>{message}</level>",
+    )
+
+
 def main() -> int:
+    logger.info("start buffet agent")
     agent = BuffetAgent()
     agent.run_agent()
     return 0
