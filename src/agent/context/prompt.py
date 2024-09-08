3a766319c49c375fa41986e0e584cce44d2e11bb
markshao <shaoyuliang@qq.com>
2024-09-08 23:03:02 +0800

    change to markdown style prompt

--- src/agent/context/prompt.py
+++ src/agent/context/prompt.py
@@ -1,7 +1,12 @@
-from ast import alias
+import os
 from agent.context.context import AgentContext
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import PromptTemplate
 
+prompt_fpath = os.path.join(os.path.dirname(__file__),"prompts","buffett_agent_prompt_template.txt")
+prompt = PromptTemplate.from_file(template_file=prompt_fpath)
+
+    
 
 class PromptBuilder:
     SYSTEM_PROMPT = """
@@ -17,10 +22,24 @@ some constrains
 """
 
     @classmethod
-    def next_prompt_msgs(cls, ctx: AgentContext):
+    def next_prompt_msgs_v1(cls, ctx: AgentContext):
         messages = []
         messages.append(SystemMessage(content=cls.SYSTEM_PROMPT))
         messages.append(HumanMessage(content=ctx.model_dump_json(by_alias=True)))
+        # append history llm messages
+        messages.extend(ctx.llm_logs)
+        messages.append(HumanMessage(content="what's your next thinking or action?"))
+        return messages
+    
+    @classmethod
+    def next_prompt_msgs_v2(cls, ctx: AgentContext):
+        messages = []
+        prompt_content = prompt.format(
+            total_available_money = ctx.stockActCtx.total_available_money,
+            stock_holding= ctx.stockActCtx.stock_holding,
+            interested_stock_list = ctx.stockActCtx.interested_stock_list
+        )
+        messages.append(SystemMessage(content=prompt_content))
         # append history llm messages
         messages.extend(ctx.llm_logs)
         messages.append(HumanMessage(content="what's your next thinking or action?"))
