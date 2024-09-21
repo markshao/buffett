2831ed9b4675323ef3b4282a83840886dcaf90e5
markshao <37647536@qq.com>
2024-09-21 21:02:22 +0800

    comment unnecessary code

--- src/tests/tools/test_call_function.py
+++ src/tests/tools/test_call_function.py
@@ -1,20 +1,20 @@
-import pytest
-from agent.tools.func_call.call import FunctionCallEngine
-
-
-@pytest.fixture
-def fce(request, cfg) -> FunctionCallEngine:
-    _fce = FunctionCallEngine(cfg)
-    _fce.initialize()
-    return _fce
-
-
-# def test_function_call_register(fce):
-#     fce.initialize()
-#     assert len(fce.all_registerd_functitoins()) > 0
-
-
-def test_tool_list_definition(fce):
-    # fce.initialize()
-    print("\n")
-    print(fce.tools_definitions().json())
+# import pytest
+# from agent.tools.func_call.call import FunctionCallEngine
+#
+#
+# @pytest.fixture
+# def fce(request, cfg) -> FunctionCallEngine:
+#     _fce = FunctionCallEngine(cfg)
+#     _fce.initialize()
+#     return _fce
+#
+#
+# # def test_function_call_register(fce):
+# #     fce.initialize()
+# #     assert len(fce.all_registerd_functitoins()) > 0
+#
+#
+# def test_tool_list_definition(fce):
+#     # fce.initialize()
+#     print("\n")
+#     print(fce.tools_definitions().json())
